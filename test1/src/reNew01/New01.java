f306e163c732441d7a6565650d523092095987a2
hyunjoo <hjstal@naver.com>
2023-05-03 17:12:30 +0900

    test + 1

---
+++ test1/src/reNew01/New01.java
@@ -0,0 +1,11 @@
+package reNew01;
+
+public class New01 {
+
+	public static void main(String[] args) {
+		//홍길동의 주민번호: 881002-1234567
+		//출력형태 : 생년월일 : 881002, 성별 : 남
+		String pin = "881002-1234567";
+	}
+
+}
